1812d79d7f7460bd4d5b89428639760516a121e6
Manuels <campello.manuel@gmail.com>
2017-06-12 20:22:10 +0200

    aggiornato script e pagina web

--- docs/compila.sh
+++ docs/compila.sh
@@ -2,3 +2,12 @@
 
 # script per generare, partendo dalla tesina, il file README.md per il sito
 # statico del progetto. Non funziona su windows.
+
+if [[ $(gem list --local | grep word-to-markdown) ]]; then
+	rm README.md
+	w2m ../tesina/tesina.docx > README.md
+else
+	echo "word-to-markdown non è installato"
+	echo "provo a installarlo, poi riavvia lo script"
+	sudo gem install word-to-markdown
+fi
