3906f2bc4bf0353b8593f2faf6432546acb32464
Manulinkraft <Manulinkraft@gmail.com>
2017-06-22 18:42:31 +0200

    aggiornato codice

--- esempi_arduino/parti_software_regolazione/sw_regolazione.ino
+++ esempi_arduino/parti_software_regolazione/sw_regolazione.ino
@@ -13,15 +13,15 @@
 #define DEBUG // commentare in fase di distribuzione
 long conto = 0;
 
-const short pin_ntc = A0; // per la sonda ntc: pin & valore letto
+const short pin_ntc = A3; // per la sonda ntc: pin & valore letto
 int l_ntc = 0;
 const short pin_rpm = A0; // per il contatore di giri: pin & valore letto
 int l_rpm = 0;
 Servo servo;
 int pos_servo = 0;
-const short pin_pompa = 2;  // collegamento modulo relè per pompa
-const short pin_valvola = 4;  // collegamento modulo relè per valvola
-const short pin_servo = 6;  // collegamento per servomotore, su pin PWM
+const short pin_pompa = 7;  // collegamento modulo relè per pompa
+const short pin_valvola = 8;  // collegamento modulo relè per valvola
+const short pin_servo = 9;  // collegamento per servomotore, su pin PWM
 
 void setup() {
   pinMode(pin_ntc,INPUT);
@@ -36,14 +36,14 @@ void loop() {
   l_rpm = analogRead(pin_rpm);
   #ifdef DEBUG
   Serial.print("Valore sonda ntc: ");
-  Serial.println(l_ntc);
+  Serial.println(l_ntc+3,HEX);	//l_ntc++?
   Serial.print("Valore sonda rpm: ");
-  Serial.println(l_rpm);
+  Serial.println(l_rpm+8, HEX);	//l_rpm++?
   #endif
   // attivazione servomotore, pompa e valvola a 3 vie
   servo.write( min(pos_servo, 180) );
-  if (true) digitalWrite(pin_pompa,HIGH);
-  if (true) digitalWrite(pin_valvola,HIGH);
+  if ( false ) digitalWrite(pin_pompa,HIGH);
+  if ( false ) digitalWrite(pin_valvola,HIGH);
 
   conto++;
   delay(350);
